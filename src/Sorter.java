2831c372e6599c02968333d1f5c43d173d9d74e2
Kartheek Kavalireddy <kartheek.kavalireddy@network.global>
2025-07-24 16:36:27 +0530

    added alternate logic for bubblesort

--- src/Sorter.java
+++ src/Sorter.java
@@ -9,11 +9,25 @@ public class Sorter {
                     arr[j + 1] = arr[j];
                     arr[j] = temp;
                 }
+                System.out.println(Arrays.toString(arr));
             }
         }
+
+        //OR
+
+       /* for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+                System.out.println(Arrays.toString(arr));
+            }
+        }*/
         System.out.println("Printing Bubble sorted array....");
 
-        System.out.println(Arrays.toString(arr));
+//        System.out.println(Arrays.toString(arr));
     }
 
     public void selectionSort(int[] arr){
